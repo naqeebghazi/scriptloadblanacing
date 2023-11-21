12712f39528539be29c7e37a8eef427b0aaa5f85
naqeebghazi <naqeebghazi@outlook.com>
2023-11-21 10:11:59 +0000

    images

--- nginxLB.sh
+++ nginxLB.sh
@@ -1,13 +1,13 @@
 
 #!/bin/bash
 
-######################################################################################################################
+##################################################################################
 ##### This automates the configuration of Nginx to act as a load balancer
 ##### Usage: The script is called with 3 command line arguments. The public IP of the EC2 instance where Nginx is installed
 ##### the webserver urls for which the load balancer distributes traffic. An example of how to call the script is shown below:
 ##### ./configure_nginx_loadbalancer.sh PUBLIC_IP Webserver-1 Webserver-2
 #####  ./configure_nginx_loadbalancer.sh 127.0.0.1 192.2.4.6:8000  192.32.5.8:8000
-############################################################################################################# 
+##################################################################################
 
 PUBLIC_IP=$1
 firstWebserver=$2
